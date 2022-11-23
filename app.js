9c61ce380716d3371e3b483ac8df385bfb777891
Chukwuemeka Kanu <mekuseo@gmail.com>
2022-11-23 19:04:57 +0100

    full-build

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const speakers = [
     name: "Sakura Haruno",
     title: "The Medic Ninja",
     bio: "Sakura Haruno is a fictional character in the Naruto manga and anime series created by Masashi Kishimoto.",
-    image: "./images/Sakura.png",
+    image: "./images/Sakura.jpg",
   },
   {
     name: "Kakashi Hatake",
